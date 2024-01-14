a7fc98cb00baf5aac585b8ecfc13b68ddeba1bb9
rushikeshhirve <rushikeshhirve09@gmail.com>
2024-01-14 20:59:35 +0530

    code change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 const API_URL = process.env.API_URL
-
+app.set("view engine", "ejs")
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
