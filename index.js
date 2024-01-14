a671ae53fd4e5b4b465f8fde7e9aff666c2049df
rushikeshhirve <rushikeshhirve09@gmail.com>
2024-01-14 17:29:57 +0530

    port changed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 3000;
 const API_URL = process.env.API_URL
 
 app.use(express.static("public"));
